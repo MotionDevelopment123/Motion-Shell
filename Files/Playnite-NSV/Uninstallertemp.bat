608326bf303ff828b74feec86da2117b37a5edc6
Motion Development <unknown.motion123@gmail.com>
2025-02-08 20:16:57 +0000

    0.5.0

---
+++ Files/Playnite-NSV/Uninstallertemp.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+
+ECHO Uninstaller
+
+ECHO This will remove Motion-Shell and any installed modules, replacing your shell back to the default Windows Shell.
+
+ECHO Are you sure you want to continue?
+
+set /p confirm=Y/N:
+if "%confirm%"=="Y" goto Continue
+if "%confirm%"=="y" goto Continue
+if "%confirm%"=="N" goto nouninstall
+if "%confirm%"=="n" goto nouninstall
+
+:nouninstall
+CLS
+ECHO.
+ECHO. No changes have been made to Motion-Shell or its setup.
+pause
+exit
+
+:Continue
+CLS
+ECHO.
+Uninstalling Motion-Shell and any modules installed
+ECHO.
+ECHO.
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "" /f
+ECHO Registry key returned to default
+rmdir /S /Q C:\Motion-Shell
+ECHO Removed Files
+ECHO.
+ECHO.
+ECHO Motion-Shell uninstalled.
+pause
+exit
